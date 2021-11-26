e69ca9286c664419298b2643c2054a22e323f8c6
gech11 <getachewfikadu77@gmail.com>
2021-11-26 05:01:25 +0300

    initial commit

---
+++ app/Http/Controllers/AgencyController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Agency;
+use App\Http\Requests\StoreAgencyRequest;
+use App\Http\Requests\UpdateAgencyRequest;
+
+class AgencyController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreAgencyRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreAgencyRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Agency  $agency
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Agency $agency)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Agency  $agency
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Agency $agency)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateAgencyRequest  $request
+     * @param  \App\Models\Agency  $agency
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateAgencyRequest $request, Agency $agency)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Agency  $agency
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Agency $agency)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/WorkController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Work;
+use App\Http\Requests\StoreWorkRequest;
+use App\Http\Requests\UpdateWorkRequest;
+
+class WorkController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreWorkRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreWorkRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Work  $work
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Work $work)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Work  $work
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Work $work)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateWorkRequest  $request
+     * @param  \App\Models\Work  $work
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateWorkRequest $request, Work $work)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Work  $work
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Work $work)
+    {
+        //
+    }
+}
