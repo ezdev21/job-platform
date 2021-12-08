cffcfd574b007d9131f3f404d11eb2ce924b99c9
gech11 <getachewfikadu77@gmail.com>
2021-12-08 22:11:47 +0300

    job components added

---
+++ resources/js/components/job/savedJobs.vue
@@ -0,0 +1,28 @@
+<template>
+ <div>
+  <button>
+
+  </button>
+ </div>
+</template>
+<script>
+export default {
+ props:{
+  user:{
+   type:Object,
+   required:true
+  }
+ },
+ data(){
+  return{
+
+  }
+ },
+ mounted(){
+
+ },
+ methods:{
+
+ }
+}
+</script>
