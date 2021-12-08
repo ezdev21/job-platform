456c2b84ee4559fdc67270de1b1efc234b59bac7
gech11 <getachewfikadu77@gmail.com>
2021-12-08 22:59:36 +0300

    hobby feature added

---
+++ app/Http/Controllers/HobbyController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Hobby;
+use App\Http\Requests\StoreHobbyRequest;
+use App\Http\Requests\UpdateHobbyRequest;
+
+class HobbyController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreHobbyRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreHobbyRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Hobby  $hobby
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Hobby $hobby)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Hobby  $hobby
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Hobby $hobby)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateHobbyRequest  $request
+     * @param  \App\Models\Hobby  $hobby
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateHobbyRequest $request, Hobby $hobby)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Hobby  $hobby
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Hobby $hobby)
+    {
+        //
+    }
+}
