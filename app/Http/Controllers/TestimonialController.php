a90fbdfe1e16dc54901e8ce2778e226886888c31
gech11 <getachewfikadu77@gmail.com>
2021-12-08 22:28:11 +0300

    referral feature included

---
+++ app/Http/Controllers/TestimonialController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Testimonial;
+use App\Http\Requests\StoreTestimonialRequest;
+use App\Http\Requests\UpdateTestimonialRequest;
+
+class TestimonialController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreTestimonialRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreTestimonialRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Testimonial  $testimonial
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Testimonial $testimonial)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Testimonial  $testimonial
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Testimonial $testimonial)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateTestimonialRequest  $request
+     * @param  \App\Models\Testimonial  $testimonial
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateTestimonialRequest $request, Testimonial $testimonial)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Testimonial  $testimonial
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Testimonial $testimonial)
+    {
+        //
+    }
+}
