130886c37874c506f67eafd608208511b11a0e41
gech11 <getachewfikadu77@gmail.com>
2021-12-08 22:45:31 +0300

    skill components added

---
+++ resources/js/components/skill/addSkill.vue
@@ -0,0 +1,39 @@
+<template>
+ <div>
+  <input type="text" v-model="skill" @input="filterSkills" class="">
+  <div v-if="filtedSkills.length">
+    <li v-for="skill in filtedSkills" :key="skill.id">{{skill.name}}</li>
+  </div>
+ </div>
+</template>
+<script>
+export default {
+ props:{
+  user:{
+   type:Object,
+   required:true
+  }
+ },
+ data(){
+  return{
+    skill:'',
+    skills:[],
+    filtedSkills:[]
+  }
+ },
+ mounted(){
+  getSkills();
+ },
+ methods:{
+  getSkills(){
+   axios.get('/skill/all')
+   .then(res=>{
+    this.skills=res.data.skills;
+   });
+  },
+  filterSkills(){
+
+  }
+ }
+}
+</script>
---
+++ resources/js/components/skill/skills.vue
@@ -0,0 +1,34 @@
+<template>
+ <div>
+  <add-skill-component/>
+  <li v-for="skill skills" :key="skill.id"><span>{{skill.name}}</span>
+  <remove-skill-component/>
+  </li>
+ </div>
+</template>
+<script>
+export default {
+ props:{
+  user:{
+   type:Object,
+   required:true
+  }
+ },
+ data(){
+  return{
+   skills:[]
+  }
+ },
+ mounted(){
+  this.getSkills();
+ },
+ methods:{
+  getSkills(){
+   axios.get('/skill/all',{params:{user:this.user}})
+   .then(res=>{
+    this.skills=res.data.skills;
+   })
+  }
+ } 
+}
+</script>
