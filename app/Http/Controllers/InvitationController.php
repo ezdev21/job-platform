5bd435872f19339f48077bc82ad4687ec6982143
gech11 <getachewfikadu77@gmail.com>
2021-12-08 22:05:21 +0300

    invitations included

---
+++ app/Http/Controllers/InvitationController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Invitation;
+use App\Http\Requests\StoreInvitationRequest;
+use App\Http\Requests\UpdateInvitationRequest;
+
+class InvitationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreInvitationRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreInvitationRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Invitation  $invitation
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Invitation $invitation)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Invitation  $invitation
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Invitation $invitation)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateInvitationRequest  $request
+     * @param  \App\Models\Invitation  $invitation
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateInvitationRequest $request, Invitation $invitation)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Invitation  $invitation
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Invitation $invitation)
+    {
+        //
+    }
+}
