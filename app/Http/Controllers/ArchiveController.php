d71d754db62f41acf42e806833ce8e25200dbc83
gech11 <getachewfikadu77@gmail.com>
2021-12-08 22:23:07 +0300

    archive feature added

---
+++ app/Http/Controllers/ArchiveController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Archive;
+use App\Http\Requests\StoreArchiveRequest;
+use App\Http\Requests\UpdateArchiveRequest;
+
+class ArchiveController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreArchiveRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreArchiveRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Archive  $archive
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Archive $archive)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Archive  $archive
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Archive $archive)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateArchiveRequest  $request
+     * @param  \App\Models\Archive  $archive
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateArchiveRequest $request, Archive $archive)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Archive  $archive
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Archive $archive)
+    {
+        //
+    }
+}
