1f4a3a2643ed7efbb5611031a0138d4042dd2e14
gech11 <getachewfikadu77@gmail.com>
2021-12-09 22:57:38 +0300

    job watch later feature created

---
+++ resources/js/components/job/watchLater.vue
@@ -0,0 +1,30 @@
+<template>
+ <div>
+  <button @click="watchLater">
+
+  </button>
+ </div>
+</template>
+<script>
+export default {
+ props:{
+  job:{
+   type:Object,
+   required:true
+  }
+ },
+ data(){
+  return{
+
+  }
+ },
+ methods:{
+  watchLater(){
+   axios.post('/job/watchlater',{job:this.job})
+   .then(res=>{
+
+   });
+  }
+ }
+}
+</script>
