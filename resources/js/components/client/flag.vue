edf4f350df5cff65c5ca6df18a9bb107c5acb1a2
gech11 <getachewfikadu77@gmail.com>
2021-12-09 23:04:14 +0300

    flag inappropriate client component

---
+++ resources/js/components/client/flag.vue
@@ -0,0 +1,30 @@
+<template>
+ <div>
+  <button @click="flag">
+   flag as in appropriate
+  </button>
+ </div>
+</template>
+<script>
+export default {
+ props:{
+  client:{
+   type:Object,
+   required:true
+  }
+ },
+ data(){
+  return{
+
+  }
+ },
+ methods:{
+  flag(){
+   axios.post('/client/flag',{client:this.client})
+   .then(res=>{
+
+   });
+  }
+ }
+}
+</script>
