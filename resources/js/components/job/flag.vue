5e3bac6d2fbe0861b77235a1c1e037e508d116fd
gech11 <getachewfikadu77@gmail.com>
2021-12-09 22:59:20 +0300

    job flag as inappropriate

---
+++ resources/js/components/job/flag.vue
@@ -0,0 +1,30 @@
+<template>
+ <div>
+  <button @click="flag">
+   flag as inappropriate
+  </button>
+ </div>
+</template>
+<script>
+export default {
+ props:{
+  job:{
+   type:Object,
+   required:true
+  }
+ },
+ data(){
+  return{
+
+  }
+ },
+ methods:{
+  flag(){
+   axios.post('/job/flag',{job:this.job})
+   .then(res=>{
+
+   });
+  }
+ }
+}
+</script>
