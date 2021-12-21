585763853c5927509fb2a95845888d765c724e4d
gech11 <getachewfikadu77@gmail.com>
2021-12-21 11:07:45 +0300

    job search component implemented

--- resources/js/components/navigation/search.vue
+++ resources/js/components/navigation/search.vue
@@ -1,6 +1,13 @@
 <template>
  <div>
-
+   <form @submit.prevent="search">
+    <input type="text" v-model="query" @input="filterCatgories" class="text-xl text-first text-xl p-2 rounded-lg border-2 border-gray-300 w-96" required placeholder="search jobs">
+   </form>
+   <div v-if="filtetedCategories" class="bg-white">
+    <ul>
+      <li v-for="category in categories" :key="category.id">{{category.name}}</li>
+    </ul>
+   </div>
  </div>
 </template>
 <script>
@@ -8,13 +15,22 @@
   data(){
    return{
     query:'',
-    categories:[]
+    categories:[],
+    filteredCategories:[]
    }
   },
   mounted(){
    
   },
   methods:{
+    filterCategories(){
+      this.filteredCategories=[];
+      this.Categories.forEach(category=>{
+        if(category.name.toLowerCase().match(this.query.toLowerCase())){
+          this.filteredCategories.push(doctor);
+        }
+      });
+    },
     search(){
      document.getElementById('searchQuery').value=this.query;
      document.getElementById('search-form').submit();
