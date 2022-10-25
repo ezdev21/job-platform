1ca3b66efd40c06734028f052cbd2749752f53d7
ezra02 <ezrafikadu02@gmail.com>
2022-10-25 17:47:18 +0300

    resume feature integrated

---
+++ app/Http/Controllers/ResumeController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreResumeRequest;
+use App\Http\Requests\UpdateResumeRequest;
+use App\Models\Resume;
+use Illuminate\Support\Facades\Storage;
+
+class ResumeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreResumeRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreResumeRequest $request)
+    {
+        try {
+            $request->file->storeAs('resume',auth()->id,'public');
+            return response()->json([
+                'message'=>'resume uploaded successfully'
+            ]);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'message'=>'error occured whcle uploading resume'
+            ]);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Resume  $resume
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Resume $resume)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Resume  $resume
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Resume $resume)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateResumeRequest  $request
+     * @param  \App\Models\Resume  $resume
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateResumeRequest $request, Resume $resume)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Resume  $resume
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Resume $resume)
+    {
+        $resume->delete();
+        Storage::delete("/resume/{$resume->id}.pdf");
+    }
+}
